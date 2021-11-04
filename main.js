95852a1198a17b0c1a010a86e6a78fca1c89fb9f
Emmanuel  Martinez <emmamartinzwc@gmail.com>
2021-11-04 18:46:13 -0300

    Agrega sonidos
    Sonido distintivo por cada cuadro al hacerle  click

--- main.js
+++ main.js
@@ -1,10 +1,50 @@
 let secuenciaMaquina = [];
 let secuenciaUsuario = [];
+let ronda = 0;
 
-const startButton = document.querySelector('#startButton');
+const botonComenzar = document.querySelector(".js-comenzar");
+const info = document.querySelector(".info-seccion");
 
-function startGame() {
-    startButton.className = 'hidden';
+function activarCuadrado(color) {
+  const cuadrado = document.querySelector(`[data-cuadrado='${color}']`);
+  const sonido = document.querySelector(`[data-sonido='${color}']`);
+
+  cuadrado.classList.add("activated");
+  sonido.play();
+
+  setTimeout(() => {
+    tile.classList.remove("activated");
+  }, 300);
 }
 
-startButton.addEventListener('click', startGame);
+function jugarRonda(siguienteSecuencia) {
+  siguienteSecuencia.forEach((color, i) => {
+    setTimeout(() => {
+      activarCuadrado(color);
+    }, (i + 1) * 600);
+  });
+}
+
+function comenzarSiguientePaso() {
+  const cuadrados = ["rojo", "verde", "azul", "amarillo"];
+  const aleatorio = cuadrados[Math.floor(Math.random() * cuadrados.length)];
+
+  return aleatorio;
+}
+
+function comenzarSiguienteRonda() {
+  ronda++;
+
+  const siguienteSecuencia = [...secuenciaMaquina];
+  siguienteSecuencia.push(comenzarSiguientePaso());
+  jugarRonda(siguienteSecuencia);
+}
+
+function iniciarJuego() {
+  botonComenzar.classList.add("oculto");
+  info.classList.remove("oculto");
+  info.textContent = "Turno de la maquina";
+  comenzarSiguienteRonda();
+}
+
+botonComenzar.addEventListener("click", iniciarJuego);
